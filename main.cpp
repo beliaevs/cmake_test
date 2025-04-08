605e042537440983df3c83ccafbc9b4ca950426b
Alexander Beliaev <beliaevs@gmail.com>
2025-04-08 15:49:49 +0300

    debug/release check

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "f.h"
 #include<iostream>
 
+bool isRelease()
+{
+#ifdef NDEBUG 
+    return true;
+#else
+    return false;
+#endif
+}
+
 int main()
 {
+    std::cout << (isRelease() ? "Release build" : "Debug build") << "\n";
     std::cout << "Hello!\n";
     std::cout << "f() = " << f() << "\n";
     std::cout << print_cont(get_primes()) << "\n";
